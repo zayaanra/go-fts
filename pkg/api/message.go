34c70b8365a42054235c7f4d56fb16b26a57adbd
Zayaan <zayaanra@buffalo.edu>
2026-01-02 16:13:59 -0500

    Added message types

--- pkg/api/message.go
+++ pkg/api/message.go
@@ -3,12 +3,14 @@ package api
 const (
 	INITIAL_CONNECT = 0 // Signifies the message is for sending a passphrase to relay server for PAKE
 	CONFIRMATION = 1 // Signifies that both clients (A and B) have connected to the relay server so that the PAKE procss can begin
-	SEND_FILE = 2 // Signifies the message is for sending data to end user
+	SHARE_PBK = 2 // Signifies that both parties want to share their public key
 )
 
 type Message struct {
 	Protocol int
 
 	Session_ID string
-	Data       []byte
+	Data []byte
+
+	PB_Key []byte
 }
