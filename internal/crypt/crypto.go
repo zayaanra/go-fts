4dcb84e6c227ccbd9ce9d18b33689eaf419f9891
Zayaan <rahmanzayaan43@gmail.com>
2026-01-09 22:13:29 -0500

    Renamed sec module to crypt

---
+++ internal/crypt/crypto.go
@@ -0,0 +1,51 @@
+package crypt
+
+import (
+	"bytes"
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "fmt"
+    "io"
+)
+
+func EncryptAES(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	padding := aes.BlockSize - len(plaintext) % aes.BlockSize
+	padtext := append(plaintext, bytes.Repeat([]byte{byte(padding)}, padding)...)
+
+	ciphertext := make([]byte, aes.BlockSize + len(padtext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], padtext)
+
+	return ciphertext, nil
+}
+
+func DecryptAES(ciphertext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, fmt.Errorf("Ciphertext too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	padding := int(ciphertext[len(ciphertext)-1])
+	return ciphertext[:len(ciphertext)-padding], nil
+}
